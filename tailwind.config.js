156c29fd18a7d6b057fb7bcae826125f76326f6c
markgrahamdawson <markgrahamdawson@gmail.com>
2022-10-31 14:34:36 +0000

    base nuxt project with fonts set up

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      permark: ['Permanent Marker'],
+      lbtk: ['Loved by the King'],
+      josan: ['Josefin Sans'],
+      amatic: ['Amatic SC'],
+    },
+    variants: {},
+    plugins: []
+  }
+};
+
